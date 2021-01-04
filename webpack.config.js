67f2b980614d18c9f78f3fc007518517db21daa7
Cristin O'Connor <her@cristin.io>
2021-01-04 12:44:06 -0700

    Changed prod entry point

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: './src/index.js',
-  externals: [nodeExternals()],
+  externals: [nodeExternals()], 
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
